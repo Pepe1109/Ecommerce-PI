56a824f08b34f528a06ce8e5a4b09e3559903357
Pepe1109 <palemanyfct@gmail.com>
2025-09-16 16:55:18 +0200

    afegit camp telèfon i validació

--- frontend/validacio.js
+++ frontend/validacio.js
@@ -1,7 +1,9 @@
 document.getElementById("formContacte").addEventListener("submit", function(event) {
     const nom = document.getElementById("nom").value;
     const email = document.getElementById("email").value;
-  
+    const telefon = document.getElementById("telefon").value;
+    const regexTelefon = /^[0-9]{9}$/;
+
     if (nom.length < 3) {
       alert("El nom ha de tindre almenys 3 caràcters.");
       event.preventDefault();
@@ -11,5 +13,11 @@ document.getElementById("formContacte").addEventListener("submit", function(even
       alert("El correu ha de contindre un '@'.");
       event.preventDefault();
     }
+    
+    if (!regexTelefon.test(telefon)) {
+      alert("El telèfon ha de tindre exactament 9 dígits numèrics.");
+      event.preventDefault();
+    }
+
   });
   
